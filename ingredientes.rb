697335b08574bce4f0b368fe71ebc37d596f26dd
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-30 20:05:56 -0400

    second commit

---
+++ ingredientes.rb
@@ -0,0 +1,11 @@
+ingrediente = ARGV[0]
+ingredientes_pizza = ['tomate', 'espinaca', 'salsa', 'queso']
+
+puts ingredientes_pizza.include?(ingrediente) ? "existe" : "no existe" 
+
+# if (ingredientes_pizza.include?(ingrediente))
+#     puts 'existe'
+# else
+#     puts 'no existe'
+# end
+
